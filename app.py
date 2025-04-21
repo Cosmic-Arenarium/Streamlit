13fb7174c9436bbc0e17c8a6484580f021ab3fb4
Cosmic-Arenarium <60487272+Cosmic-Arenarium@users.noreply.github.com>
2025-04-21 21:06:05 +0000

    Removing lines

--- app.py
+++ app.py
@@ -5,7 +5,3 @@ st.title("My First App")
 name = st.text_input("What's your name?")
 if name:
     st.write(f"Hello, {name}!")
-
-name2 = st.text_input("What's your name2?")
-if name2:
-    st.write(f"Hello, {name2}!")
