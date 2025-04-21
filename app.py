55e244cc566606fac0ff6a6f56dfc262686978e1
Cosmic-Arenarium <60487272+Cosmic-Arenarium@users.noreply.github.com>
2025-04-21 20:56:42 +0000

    Rating Prediction

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ st.title("My First App")
 name = st.text_input("What's your name?")
 if name:
     st.write(f"Hello, {name}!")
+
+name2 = st.text_input("What's your name2?")
+if name2:
+    st.write(f"Hello, {name2}!")
